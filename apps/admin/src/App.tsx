d85aac18dcb74612c828607961a81dd25b5b0c53
Raman Kaur <botanymyselfplants@gmail.com>
2026-07-21 19:33:06 +0530

    Update App.tsx

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AdminShell } from "./ui/AdminShell";
-import { Dashboard } from "./pages/Dashboard";
-import { Products } from "./pages/Products";
-import { AddProduct } from "./pages/AddProduct";
+import AdminShell from "./ui/AdminShell";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/editProduct";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/new" element={<AddProduct />} />
+        <Route path="/products/:id/edit" element={<EditProduct />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AdminShell>
